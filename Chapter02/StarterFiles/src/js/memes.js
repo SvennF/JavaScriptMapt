c904ddb8b1e4a1f604fe9fcc402b4ecda0bfaaa3
Svenn Finnvold <svennfinn@gmail.com>
2018-09-22 13:45:08 +0200

    start work on cap2

---
+++ Chapter02/StarterFiles/src/js/memes.js
@@ -0,0 +1,15 @@
+import './general';
+console.log('Meme js file');
+const deviceWidth = window.innerWidth;
+class Memes{
+    constructor(){
+        console.log('inside meme class');
+        this.$canvas = document.querySelector('#imgCanvas');
+        this.$topTextInput = document.querySelector('#topText');
+        this.$bottomTextInput = document.querySelector('#bottomText');
+        this.$imageInput = document.querySelector('#image');
+        this.$downloadButton = document.querySelector('#downloadMeme');
+    }
+}
+
+new Memes();
